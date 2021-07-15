1b3b4d7d031665831df624d8931e793a67a039a4
mchavis1001 <mchavis1001@gmail.com>
2021-07-14 20:48:01 -0400

    updated api_keys

--- api_keys.py
+++
@@ -1,3 +0,0 @@
-api = 'f1f9f5c76d5a0a2e82d36265733fe575'
-secret = 'UiwWhZSZ2gV4nssip7NuWlbBJxzO8q+P1AE+L9Cx0iBAdmkSDJKlFwCO/I1FAKuK2Sc5M20z/2ErXjI098ZgCQ=='
-passphrase = 'f1blb4uzw6s'
--- run.py
+++ run.py
@@ -1,11 +1,15 @@
 import os
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 from CB_api import CoinbaseClient
-from api_keys import api, secret, passphrase
 import requests
 import json
 
-test = CoinbaseClient(api, secret, passphrase)
-print(test.historical_data('DOGE-USD', 86400))
+load_dotenv()
+cb_api_key = os.getenv("cb_api")
+cb_secret = os.getenv("cb_secret")
+cb_passphrase = os.getenv("cb_passphrase")
+
+test = CoinbaseClient(cb_api_key, cb_secret, cb_passphrase)
+print(test.historical_data('BTC-USD', 86400))
 
 
