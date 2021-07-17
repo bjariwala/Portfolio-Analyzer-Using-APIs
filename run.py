731537adf03bcb99a5e334fadcc596f12294b4f8
Samuel Moore <celticcontracting@yahoo.com>
2021-07-17 14:11:10 -0400

    created env file and loaded on run.py

--- run.py
+++ run.py
@@ -3,12 +3,19 @@ from dotenv import load_dotenv
 from CB_api import CoinbaseClient
 import requests
 import json
+import sys
+from os.path import join, dirname
+from dotenv import load_dotenv
+from pathlib import Path
 
-load_dotenv()
-cb_api_key = os.getenv("cb_api")
+dotenv_path = join(dirname(__file__), './/.idea//inspectionProfiles//.env')
+print(dotenv_path)
+load_dotenv(Path(dotenv_path))
+
+cb_api_key = os.getenv("cb_api_key")
 cb_secret = os.getenv("cb_secret")
 cb_passphrase = os.getenv("cb_passphrase")
-
+print('-----', cb_api_key, cb_secret, cb_passphrase)
 test = CoinbaseClient(cb_api_key, cb_secret, cb_passphrase)
 print(test.historical_data('BTC-USD', 86400))
 
